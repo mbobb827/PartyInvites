8294e6db64ef3f7e08755078e84f9525ca5b797f
Mark Bobbitt <mark.bobbitt@outlook.com>
2020-02-27 13:49:19 -0600

    test

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = (hour < 12 ? "Good Morning" : "Good Afternoon");
+
+            return View("MyView");
+        }
+
+
+        public ViewResult RsvpForm() {
+            
+            return View();
+        }
+
+
+    }
+}
